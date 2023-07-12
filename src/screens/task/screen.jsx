bea9dfbe0b023e645dc43f3e0023229a58400222
FlorLizardo <florencializardo7@gmail.com>
2023-07-12 17:00:41 +0200

    feat(ui): add UI TaskScreen

--- src/screens/task/screen.jsx
+++ src/screens/task/screen.jsx
@@ -1,9 +1,47 @@
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native';
+import { CreateTask} from '../../components';
+
+const styles = StyleSheet.create({
+  containerBody: {
+    flex: 1,
+    marginTop: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF'
+   },
+   tarea: {
+    color: '#FFFFFF',
+    fontSize: 18,
+    fontWeight: '400',
+    lineHeight: 36,
+    letterSpacing: 0.54
+   },
+   containerTarea: {
+    backgroundColor: '#7141FA',
+    width: 227,
+    height: 37,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 40,
+    borderRadius: 5,
+    shadowColor: "#B7B7B7",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 3.84,
+    elevation: 4,
+   },
+});
 
 function TaskScreen() {
   return (
-    <View>
-      <Text>Create Task Screen</Text>
+    <View style={styles.containerBody}>
+      <View style={styles.containerTarea}>
+        <Text style={styles.tarea}>Nueva Tarea</Text>
+      </View>
+      <CreateTask/>
     </View>
   )
 }
