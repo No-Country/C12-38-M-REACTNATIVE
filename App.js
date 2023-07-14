1c4fab9a9652fa008039f203cc2ea75a0281a6f5
VickyGB <vickygbonorino@gmail.com>
2023-07-14 19:42:07 -0300

    feat: add log in page

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
-import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
-import { GestureHandlerRootView } from 'react-native-gesture-handler'
-import NavigationProvider from './src/navigation/navigation'
+// import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
+// import { GestureHandlerRootView } from 'react-native-gesture-handler'
+// import NavigationProvider from './src/navigation/navigation'
+
+// function App() {
+//   return (
+//     <GestureHandlerRootView style={{ flex: 1 }}>
+//       <BottomSheetModalProvider>
+//         <NavigationProvider />
+//       </BottomSheetModalProvider>
+//     </GestureHandlerRootView>
+//   )
+// }
+
+// export default App;
+
+import React from "react";
+import LogIn_Page from "../C12-38-M-REACTNATIVE/src/screens/logIn/logIn";
 
 function App() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <BottomSheetModalProvider>
-        <NavigationProvider />
-      </BottomSheetModalProvider>
-    </GestureHandlerRootView>
+    <LogIn_Page />
   )
 }
 
 export default App;
+
+
