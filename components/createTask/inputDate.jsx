816c34c146ff754e9c9495028716963ea250fb28
FlorLizardo <florencializardo7@gmail.com>
2023-07-25 13:49:21 +0200

    style: add clock and calendar

---
+++ components/createTask/inputDate.jsx
@@ -0,0 +1,98 @@
+import { Entypo } from '@expo/vector-icons'
+import { useState } from 'react'
+import { StyleSheet, TextInput, View, Pressable, Platform } from 'react-native'
+import DateTimePicker  from '@react-native-community/datetimepicker';
+
+const styles = StyleSheet.create({
+ textInput: {
+  color: '#000',
+  width: 303,
+  height: 40,
+  borderColor: '#D9D9D9',
+  borderWidth: 2,
+  borderRadius: 12,
+  fontSize: 14,
+  fontWeight: '400',
+  backgroundColor: '#FFFFFF',
+  paddingStart: 16,
+  paddingTop: 8,
+  paddingEnd: 16,
+  paddingBottom: 8,
+  marginTop: 25
+},
+icon: {
+  position: 'absolute',
+  alignSelf: 'flex-end',
+  right: 14,
+  marginTop: 32
+}
+})
+
+function InputDate(){
+
+  const [date, setDate] = useState(new Date());
+  const [showPicker, setShowPicker] = useState(false);
+  const [dayInput, setDayInput] = useState('');
+
+  const toggleDatepicker = () => {
+   setShowPicker(!showPicker);
+  };
+  
+  const formatDate = (rawDate) => {
+    let date = new Date(rawDate);
+
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+
+    month = month < 10 ? `0${month}` : month;
+    day = day < 10 ? `0${day}` : day;
+
+    return `${day}/${month}/${year}`
+  }
+
+  const onChange = ({type}, selectedDate) => {
+   if (type == 'set') {
+   const currentDate = selectedDate;
+   setDate(currentDate);
+
+     if(Platform.OS === 'android') {
+      toggleDatepicker();
+      setDayInput(formatDate(currentDate));
+      }
+      } else {
+         toggleDatepicker();
+      }
+    };
+
+ return(
+  <View>
+    <View>
+      {showPicker && (
+      <DateTimePicker
+        mode='date'
+        display='spinner'
+        value={date}
+        onChange={onChange}
+     />
+       )}
+      {!showPicker &&(
+      <Pressable onPress={toggleDatepicker} >
+         <TextInput
+          placeholder='El día'
+          placeholderTextColor='#B7B7B7'
+          style={styles.textInput}
+          value={dayInput}
+          onChangeText={setDayInput}
+          editable={false}
+          
+         />
+         <Entypo style={styles.icon} name='calendar' size={24} color='#B7B7B7' />
+      </Pressable>
+      )}
+   </View>
+  </View>
+ )
+}
+
+export default InputDate
---
+++ components/createTask/inputTime.jsx
@@ -0,0 +1,98 @@
+import { AntDesign} from '@expo/vector-icons'
+import { useState } from 'react'
+import { StyleSheet, TextInput, View, Pressable, Platform } from 'react-native'
+import DateTimePicker  from '@react-native-community/datetimepicker';
+
+const styles = StyleSheet.create({
+ textInput: {
+  color: '#000',
+  width: 303,
+  height: 40,
+  borderColor: '#D9D9D9',
+  borderWidth: 2,
+  borderRadius: 12,
+  fontSize: 14,
+  fontWeight: '400',
+  backgroundColor: '#FFFFFF',
+  paddingStart: 16,
+  paddingTop: 8,
+  paddingEnd: 16,
+  paddingBottom: 8,
+  marginTop: 25
+},
+icon: {
+  position: 'absolute',
+  alignSelf: 'flex-end',
+  right: 14,
+  marginTop: 32
+}
+})
+
+function InputTime(){
+
+  const [date, setDate] = useState(new Date());
+  const [showPicker, setShowPicker] = useState(false);
+  const [hour, setHour] = useState('');
+
+  const toggleDatepicker = () => {
+   setShowPicker(!showPicker);
+  };
+  
+  const formatDate = (rawDate) => {
+   let date = new Date(rawDate);
+
+   let hour = date.getHours();
+   let minutes = date.getMinutes();
+
+   hour = hour < 10 ? `0${hour}` : hour;
+   minutes = minutes < 10 ? `0${minutes}` : minutes;
+
+   return `${hour}:${minutes}`
+ }
+
+  const onChange = ({type}, selectedHour) => {
+   if (type == 'set') {
+   const currentHour = selectedHour;
+   setDate(currentHour);
+
+     if(Platform.OS === 'android') {
+      toggleDatepicker();
+      setHour(formatDate(currentHour));
+      }
+      } else {
+         toggleDatepicker();
+      }
+    };
+
+ return(
+  <View>
+    <View>
+      {showPicker && (
+      <DateTimePicker
+        mode='time'
+        display='spinner'
+        value={date}
+        onChange={onChange}
+        is24Hour= {true}
+     />
+       )}
+      {!showPicker &&(
+      <Pressable onPress={toggleDatepicker} >
+         <TextInput
+          placeholder='A las'
+          placeholderTextColor='#B7B7B7'
+          style={styles.textInput}
+          value={hour}
+          onChangeText={setHour}
+          editable={false}
+         />
+         <AntDesign style={styles.icon} name='clockcircleo' size={24} color='#B7B7B7' />
+      </Pressable>
+      )}
+   </View>
+
+  </View>
+ )
+}
+
+export default InputTime
